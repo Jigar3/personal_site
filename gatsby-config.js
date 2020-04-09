34c55e1729663050b0834677a960178c0dea9a0a
Jigar <chavada.jigar.3@gmail.com>
2020-04-10 01:49:51 +0530

    changed code

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     siteUrl: "https://jigarc.netlify.com/",
     siteDescription: "Portfolio website of Jigar Chavada",
     siteLanguage: "en",
-    siteImage: "string",
+    siteImage: "./static/banner.jpg",
     author: "Jigar Chavada",
   },
   plugins: [
